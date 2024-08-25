9a9b07c6124d5572b3763037f078bca8d0597ac0
Miyoko Shimura <120026227+miyoko-shimura@users.noreply.github.com>
2024-08-25 08:01:23 +0100

    Update chatbotapp.py

--- chatbotapp.py
+++ chatbotapp.py
@@ -7,10 +7,10 @@ with st.sidebar:
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("💬 Chatbot")
-st.caption("🚀 A Streamlit chatbot powered by OpenAI")
+st.title("💬 Miyoko Chatbot")
+st.caption("🚀 A Streamlit chatbot powered by OpenAI.")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you???"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
